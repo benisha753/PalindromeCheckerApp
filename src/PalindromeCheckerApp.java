272bcff3e48c1a98b38d2488e71429b1e90fc5c2
gayathrisathish <bm3524@srmist.edu.in>
2026-02-27 16:06:29 +0530

    UC1:Hardecoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,18 @@
 public class PalindromeCheckerApp {
     public static void main(string[] args){
         System.out.println("Welcome to palindrome Checker App");
+        String input = "madam";
+        boolean isPalindrome= true;
+        for(int i=0;i<input.length()/2;i++){
+            if(input.charAt(i)!=input.chatAt(input.length()-1-i)){
+                is palindrome=false;
+                break;
+            }
+        }
+        if (isPalindrome) {
+            System.out.println(word + " is a Palindrome.");
+        } else {
+            System.out.println(word + " is not a Palindrome.");
+        }
     }
 }

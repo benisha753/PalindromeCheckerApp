14ed9c55b28e34ea0b29974b60866889df453467
gayathrisathish <bm3524@srmist.edu.in>
2026-02-27 15:35:31 +0530

    changes

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,5 @@
 public class PalindromeCheckerApp {
-     public static void main(string[],args){
+     public static void main(string[] args){
          System.out.println("Welcome to palindrome Checker App");
      }
 }
